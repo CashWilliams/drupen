ebc58fa31cdf76a80fbc09e813a43c97b95bb1e6
Kris <kris@meridian-ds.com>
2016-08-18 12:04:35 -0500

    added a small message for once we attempt to get views working

--- src/RouteHandler/Views.php
+++ src/RouteHandler/Views.php
@@ -14,6 +14,7 @@ class Views implements RouteHandlerInterface {
 
   public function applies(Route $route) {
     return FALSE;
+    // check $route->getDefaults()['view_id']
   }
 
   public function getUrls(RouteCollection $collection) {
